be56e3b106242d07a3ca98392662c32660f900d4
Leo <yinlei212@gmail.com>
2025-10-02 20:49:06 +0800

    Initial Commit

---
+++ subtitle_correct/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  subtitle_correct
+//
+//  Created by Leo Y on 2025/10/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ subtitle_correct/subtitle_correctApp.swift
@@ -0,0 +1,17 @@
+//
+//  subtitle_correctApp.swift
+//  subtitle_correct
+//
+//  Created by Leo Y on 2025/10/2.
+//
+
+import SwiftUI
+
+@main
+struct subtitle_correctApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
